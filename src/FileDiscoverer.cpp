fc9a9628f2ecc2be39f82fd24929fe5e5e71c139
archig97 <archismitaghosh@yahoo.com>
2026-03-04 16:41:50 -0500

    aded check to see if input directory exists

--- src/FileDiscoverer.cpp
+++ src/FileDiscoverer.cpp
@@ -4,6 +4,10 @@
 namespace fs = std::filesystem;
 
 std::vector<Task> FileDiscoverer::discover(const std::string& root, TaskType type) {
+    if (!std::filesystem::exists(root)) {
+        throw std::runtime_error("Directory does not exist: " + root);
+    }
+
     std::vector<Task> tasks;
 
     for (const auto& entry : fs::recursive_directory_iterator(root)) {
